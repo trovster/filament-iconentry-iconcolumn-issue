ed820af85d46c3877246be9cc03a3e3107985f8d
Hugh Messenger <hugh.messenger@gmail.com>
2026-01-03 11:30:53 -0600

    Update ItemsRelationManager.php

--- app/Filament/Resources/Users/RelationManagers/ItemsRelationManager.php
+++ app/Filament/Resources/Users/RelationManagers/ItemsRelationManager.php
@@ -2,6 +2,10 @@
 
 namespace App\Filament\Resources\Users\RelationManagers;
 
+use App\Filament\Resources\Items\ItemResource;
+use App\Filament\Resources\Items\Schemas\ItemForm;
+use App\Filament\Resources\Items\Schemas\ItemInfolist;
+use App\Filament\Resources\Items\Tables\ItemsTable;
 use App\Filament\Resources\UserPivots\Schemas\UserPivotForm;
 use App\Filament\Resources\UserPivots\Schemas\UserPivotInfolist;
 use App\Filament\Resources\UserPivots\Tables\UserPivotsTable;
@@ -14,21 +18,23 @@ class ItemsRelationManager extends RelationManager
 {
     protected static string $relationship = 'items';
 
-    protected static ?string $relatedResource = UserPivotResource::class;
+    protected static ?string $relatedResource = ItemResource::class;
 
     public function form(Schema $schema): Schema
     {
-        return UserPivotForm::configure($schema);
+        return ItemForm::configure($schema);
     }
 
     public function infolist(Schema $schema): Schema
     {
-        return UserPivotInfolist::configure($schema);
+//        return UserPivotInfolist::configure($schema);
+        return ItemInfolist::configure($schema);
     }
 
     public function table(Table $table): Table
     {
-        return UserPivotsTable::configure($table);
+//        return UserPivotsTable::configure($table);
+        return ItemsTable::configure($table);
     }
 
     public function isReadOnly(): bool
