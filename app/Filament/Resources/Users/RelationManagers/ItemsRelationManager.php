b24404bd46e8b35680d1a35a44aee87984dadbaf
Trevor Morris <code@trovster.com>
2026-01-03 17:41:49 +0000

    fix: items pivot relationship

--- app/Filament/Resources/Users/RelationManagers/ItemsRelationManager.php
+++ app/Filament/Resources/Users/RelationManagers/ItemsRelationManager.php
@@ -2,19 +2,23 @@
 
 namespace App\Filament\Resources\Users\RelationManagers;
 
+use App\Filament\Resources\Items\ItemResource;
 use App\Filament\Resources\UserPivots\Schemas\UserPivotForm;
 use App\Filament\Resources\UserPivots\Schemas\UserPivotInfolist;
 use App\Filament\Resources\UserPivots\Tables\UserPivotsTable;
-use App\Filament\Resources\UserPivots\UserPivotResource;
+use Filament\Actions\ViewAction;
+use Filament\Infolists\Components\TextEntry;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Schemas\Schema;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class ItemsRelationManager extends RelationManager
 {
     protected static string $relationship = 'items';
 
-    protected static ?string $relatedResource = UserPivotResource::class;
+    protected static ?string $relatedResource = ItemResource::class;
 
     public function form(Schema $schema): Schema
     {
@@ -23,12 +27,36 @@ class ItemsRelationManager extends RelationManager
 
     public function infolist(Schema $schema): Schema
     {
-        return UserPivotInfolist::configure($schema);
+        return $schema->schema([
+            TextEntry::make('name'),
+            TextEntry::make('uuid')
+                ->label('UUID'),
+        ]);
     }
 
     public function table(Table $table): Table
     {
-        return UserPivotsTable::configure($table);
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('uuid')
+                    ->label('UUID'),
+                TextColumn::make('started_at')
+                    ->label('Start')
+                    ->date('d/m/Y'),
+                TextColumn::make('finished_at')
+                    ->label('Finish')
+                    ->date('d/m/Y'),
+                TextColumn::make('order')
+                    ->numeric()
+                    ->alignCenter()
+                    ->sortable(),
+                IconColumn::make('status')
+                    ->boolean(),
+            ])
+            ->recordActions([
+                ViewAction::make()->modal(),
+            ]);
     }
 
     public function isReadOnly(): bool
