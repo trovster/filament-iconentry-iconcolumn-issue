d451694161a1b3d467220484b9d9a5f867c68af4
Hugh Messenger <hugh.messenger@gmail.com>
2026-01-03 11:30:04 -0600

    Update ItemsTable.php

--- app/Filament/Resources/Items/Tables/ItemsTable.php
+++ app/Filament/Resources/Items/Tables/ItemsTable.php
@@ -6,6 +6,7 @@ use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -16,6 +17,20 @@ class ItemsTable
         return $table
             ->columns([
                 TextColumn::make('name'),
+                TextColumn::make('uuid')
+                    ->label('UUID'),
+                TextColumn::make('started_at')
+                    ->label('Start')
+                    ->date('d/m/Y'),
+                TextColumn::make('finished_at')
+                    ->label('Finish')
+                    ->date('d/m/Y'),
+                TextColumn::make('order')
+                    ->numeric()
+                    ->alignCenter()
+                    ->sortable(),
+                IconColumn::make('status')
+                    ->boolean(),
             ]);
     }
 }
